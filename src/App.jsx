bb13b73a73702a5cfae430b4c63b9ddda30881f3
ege aydın <egeaydinn31@gmail.com>
2024-11-11 18:10:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+import "./style/style.scss"
+
+function App() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect((e)=>{
+    axios.get("https://my-json-server.typicode.com/tahsincanpolat/pokedex/pokemons")
+    .then((response)=>{
+      if(response.status === 200){
+        setPokemons(response.data)
+      }
+    })
+      .catch((e)=>{
+        console.log(e)
+    })
+  },[])
+  return (
+    <>
+    
+      <div>
+        {
+          pokemons.map((pokemon,key)=>{
+            return(
+                 <div key={key} className='Pokemons-Card'>
+                <img src={pokemon.image.thumbnail} alt={pokemon.name} />
+                <p>{pokemon.name}</p>
+              </div>
+                
+             
+            )
+          })
+        }
+      </div>
+
+    </>
+  )
+}
+
+export default App
